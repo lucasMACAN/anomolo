8eeca18653872a2da3f53d82498afca92d12882d
kkAndrade <andeilsonc648@gmail.com>
2026-05-31 01:23:02 -0300

    Update main.py
    tomei a liberdade de mudar completamente a base do jogo, apenas estruturei.

--- main.py
+++ main.py
@@ -1,50 +1,84 @@
-import os
+# CHAMEI A BIBLIOTECA DE FUNÇÕES ESPECÍFICAS DO JOGO
+import Funcoes_jogo
 
-local = "quarto"
+# CLASSE PERSONAGEM, CRIAÇÃO DE PERSONAGEM
+# E INTERAÇÃO COM O CENÁRIO
+class PERSONAGEM:
+    def __init__(self, nome):
+        self.nome = nome
+        self.sanidade = 100
+        self.escolha = 0
 
-while True:
-    os.system("clear")
-
-    if local == "quarto":
+    # FUNÇÃO DECISAO_QUARTO(): FUNÇÃO USADA PARA DECIDIR O QUE
+    # O USUÁRIO VAI FAZER QUANDO ESTIVER DENTRO DO QUARTO
+    def DECISAO_QUARTO(self):
+        Funcoes_jogo.LIMPAR()
         print("Você acorda em um quarto desconhecido.")
-        print("\n1 - Abrir a porta")
-        print("2 - Olhar o espelho")
+        input("aperte ENTER para continuar")
+        Funcoes_jogo.LIMPAR()
 
-        escolha = input("> ")
+        print("-------------MENU-------------")
+        print("(1) ir para o CORREDOR")
+        print("(2) olhar o ESPELHO\n")
+        self.escolha = int(input("> "))
+        Funcoes_jogo.LIMPAR()   # PARA GARANTIR A LIMPEZA DO TERMINAL (NECESSÁRIO)
 
-        if escolha == "1":
-            local = "corredor"
+        if self.escolha == 2:
+                Funcoes_jogo.ESPELHO()
+                self.escolha = 0
 
-        elif escolha == "2":
-            print("\nSeu reflexo sorri.")
-            input("\nENTER para continuar.")
-
-    elif local == "corredor":
+    # FUNÇÃO DECISAO_CORREDOR(): FUNÇÃO USADA PARA DECIDIR O QUE
+    # O USUÁRIO VAI FAZER QUANDO ESTIVER NO CORREDOR 
+    def DECISAO_CORREDOR(self):
+        Funcoes_jogo.LIMPAR()
         print("Um corredor longo e silencioso.")
+        input("aperte ENTER para continuar")
+        Funcoes_jogo.LIMPAR()
 
-        print("\n1 - Voltar ao quarto")
-        print("2 - Ir para a cozinha")
+        print("-------------MENU-------------")
+        print("(1) voltar ao QUARTO")
+        print("(2) ir para a COZINHA\n")
+        self.escolha = int(input("> "))
+        Funcoes_jogo.LIMPAR()   # PARA GARANTIR A LIMPEZA DO TERMINAL (NECESSÁRIO)
 
-        escolha = input("> ")
-
-        if escolha == "1":
-            local = "quarto"
-
-        elif escolha == "2":
-            local = "cozinha"
-
-    elif local == "cozinha":
+        if self.escolha == 1:
+            self.DECISAO_QUARTO()
+            
+        elif self.escolha == 2:
+            self.DECISAO_COZINHA()
+    
+    # FUNÇÃO DECISAO_COZINHA(): FUNÇÃO USADA PARA DECIDIR O QUE
+    # O USUÁRIO VAI FAZER QUANDO ESTIVER DENTRO DA COZINHA
+    def DECISAO_COZINHA(self):
+        Funcoes_jogo.LIMPAR()
         print("A cozinha está vazia.")
+        input("aperte ENTER para continuar")
+        Funcoes_jogo.LIMPAR()
 
-        print("\n1 - Voltar")
-        print("2 - Abrir a geladeira")
+        print("-------------MENU-------------")
+        print("(1) voltar ao CORREDOR")
+        print("(2) abrir a GELADEIRA\n")
+        self.escolha = int(input("> "))
+        Funcoes_jogo.LIMPAR()   # PARA GARANTIR A LIMPEZA DO TERMINAL (NECESSÁRIO)
 
-        escolha = input("> ")
+        if self.escolha == 1:
+            self.DECISAO_CORREDOR()
 
-        if escolha == "1":
-            local = "corredor"
+        elif self.escolha == 2:
+            print("Há apenas um bilhete, está escrito: Você já esteve aqui antes.")
+            input("aperte ENTER para continuar")
 
-        elif escolha == "2":
-            print("\nHá apenas um bilhete:")
-            print('"Você já esteve aqui antes."')
-            input("\nENTER para continuar.")
+# DECISAO() O MÉTODO RESPONSÁVEL POR CHAMAR AS FUNÇÕES PRINCIPAIS
+    def DECISAO(self):
+        # LIMPANDO O TERMINAL POR CONTA DAS INFO QUE APARECE
+        # QUANDO ELE É INICIADO
+        Funcoes_jogo.LIMPAR()
+
+        if self.escolha == 0:   # COLOQUEI ZERO APENAS PARA A FUNÇÃO "ENTENDER" QUE É UM CENÁRIO DIFERENTE
+            self.DECISAO_QUARTO()
+
+        elif self.escolha == 1:
+            self.DECISAO_CORREDOR()
+
+        elif self.escolha == 2:
+            self.DECISAO_COZINHA()
