6a79a87f933e0bab61b3827610103a26cdeb5c95
kkAndrade <andeilsonc648@gmail.com>
2026-05-31 12:41:39 -0300

    Novas funções.py
    Adicionei mais 2 funções, SALVAR() e CARREGAR().
    o SALVAR() já esta em uso e já foi implementado no jogo, mas não posso dizer o mesmo do CARREGAR()

--- Funcoes_jogo.py
+++ Funcoes_jogo.py
@@ -2,6 +2,13 @@
 # SUBSTITUTO DO "OS.SYSTEM"
 from subprocess import run
 
+# IMPORTANDO A BIBLIOTECA "JSON"
+import json
+
+# CRIEI A VARIÁVEL FORA PARA QUE O SALVAR()
+# E O CARREGAR() POSSA PEGAR O VALOR
+arquivo_salvo = "SAVE.json"
+
 # REDUZI A FUNÇÃO PARA FICAR MAIS LIMPO E PRÁTICO
 def LIMPAR():
     run('cls', shell = True) # PEDE PARA LIMPAR O TERMINAL
@@ -12,3 +19,25 @@ def ESPELHO():
     input("aperte ENTER para continuar")
     LIMPAR()
 
+def SALVAR(jogador):
+    
+    # SALVANDO OS ATRIBUTOS DO OBJETO, NESSE CASO O JOGADOR
+    with open(arquivo_salvo, "w") as save:
+        json.dump(vars(jogador), save)
+
+def CARREGAR(jogador):
+    try:
+        with open(arquivo_salvo, "r") as save:
+            dados = json.load(save) # PEGA O ARQUIVO QUE EU SALVEI E APELIDEI COMO "save"
+            LIMPAR()
+            print("Arquivo carregado com SUCESSO")
+            input("aperte ENTER para continuar") 
+            LIMPAR()
+
+    except FileNotFoundError:
+        LIMPAR()
+        print("Arquivo de save NÃO encontrado")
+        input("aperte ENTER para continuar")
+        LIMPAR()
+
+        jogador.escolha = dados['escolha']
